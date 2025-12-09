7a3725cf5c7e9827e2967a5b091c081e7c470a91
jhoviatt <your@jaredhenryoviatt@gmail.com>
2025-12-09 12:33:37 -0500

    Edits following MEPS revisions round 1

--- R/ggsetup.R
+++ R/ggsetup.R
@@ -3,15 +3,20 @@
 library(ggpubr)
 
 # define parameters for saving figures with ggpubr::ggsave()
-# set for ecography
-#Width: 945 (single column), 1476 (1.5 column) or 1961 (double column) pixels (at 300 dpi).
+# MEPS:
+# Type area = 169 x 225 mm, column width = 81 mm, gutter width: 7 mm
+# single panel figure (with less text) → 81 mm width
+# single panel figure (with more text or insets) → 105 mm width
+# two panel figure (horizontally aligned) → 169 mm width
+# two panel figure (vertically aligned) → 81 mm width
+# multiple panel figure → 169 mm width
 my_ggsave <- function(filename, plot, 
                       device = 'tiff',
                       path = './Figs/', 
                       dpi = 300, 
-                      units = 'px', 
-                      width = 945, 
-                      height = 945, 
+                      units = 'mm', 
+                      width = 81, 
+                      height = 81, 
                       bg = 'white')
 {
     ggsave(filename, plot, device = device, path = path, dpi = dpi, units = units, width = width, height = height, bg = bg)
